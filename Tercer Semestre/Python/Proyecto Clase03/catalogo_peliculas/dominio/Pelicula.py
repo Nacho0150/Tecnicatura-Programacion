b789a02a73cad9a012dd6ab4f9cd41322bfb8b49
Nacho0150 <ignacioibanez01@gmail.com>
2025-04-28 23:48:41 -0300

    Clase 04 Python

--- Tercer Semestre/Python/Proyecto Clase03/catalogo_peliculas/dominio/Pelicula.py
+++ Tercer Semestre/Python/Proyecto Clase03/catalogo_peliculas/dominio/Pelicula.py
@@ -4,3 +4,12 @@ class Pelicula:
 
     def __str__(self):
         return self.nombre
+
+    @property
+    def nombre(self):
+        return (self._nombre)
+
+    @nombre.setter
+    def nombre(self, nombre):
+        self._nombre = nombre
+
