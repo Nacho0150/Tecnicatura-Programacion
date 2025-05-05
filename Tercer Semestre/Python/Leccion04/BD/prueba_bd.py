518df16e955909b99f0b8879364a7585b1fd7ecd
Nacho0150 <ignacioibanez01@gmail.com>
2025-05-05 20:36:54 -0300

    Clase 05 Python

---
+++ Tercer Semestre/Python/Leccion04/BD/prueba_bd.py
@@ -0,0 +1,16 @@
+import psycopg2 # Esto es para poder concectarnos a Postgre
+
+conexion = psycopg2.connect(user='postgres', password='admin', host='127.0.0.1', port='5432', database='test_db')
+
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            sentencia = 'SELECT * FROM persona WHERE id_persona = %s' # Placeholder
+            id_persona = input('Digite un número para el id_persona: ')
+            cursor.execute(sentencia, (id_persona,)) # De esta manera ejecutamos la sentencia
+            registros = cursor.fetchone() # Recuperamos todos los registros que serán una lista
+            print(registros)
+except Exception as e:
+    print(f'Ocurrió un error: {e}')
+finally:
+    conexion.close()
