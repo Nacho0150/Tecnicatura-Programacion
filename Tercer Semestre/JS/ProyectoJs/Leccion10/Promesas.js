3789483930db6aa22ed42eb880cf0e19b4151a44
Nacho0150 <ignacioibanez01@gmail.com>
2025-06-11 20:32:23 -0300

    Clase10 JS.1

---
+++ Tercer Semestre/JS/ProyectoJs/Leccion10/Promesas.js
@@ -0,0 +1,60 @@
+let miPromesa = new Promise((resolver, rechazar) => {
+    let expresion = true;
+    if (expresion) {
+        // resolver('Resolvió correctamente');
+    } else {
+        // rechazar('Se produjo un error')
+    }
+});
+
+// miPromesa.then(
+    // valor => console.log(valor),
+    // error => console.log(error)
+// );
+
+// miPromesa
+    // .then(valor => console.log(valor))
+    // .catch(error => console.log(error));
+
+let promesa = new Promise((resolver) => {
+    // console.log('Iniciando promesa...');
+    setTimeout(() => {
+        resolver('Saludos desde promesa, callback, función flecha y setTimeout');
+    }, 3000);
+    // console.log('Final promesa');
+})
+
+//El llamado a la promesa utilizando setTimeout
+// promesa.then(valor => console.log(valor));
+
+//async indica que una función regresa una promesa
+async function miFuncionConPromesa() {
+    return 'Saludos con promesas y async';
+}
+
+// miFuncionConPromesa.apply().then(valor => console.log(valor));
+
+//async/await
+async function funcionConPromesaYAwait() {
+    let miPromesa = new Promise(resolver => {
+        resolver('Promesa con await');
+    });
+    console.log(await miPromesa);
+}
+
+// funcionConPromesaYAwait();
+
+//Promesas, await, async y setTimeout
+async function funcionConPromesaYAwaitTimeout() {
+    let miPromesa = new Promise(resolver => {
+        setTimeout(() => {
+            console.log('Inicio función')
+            resolver('Promesa con await y Timeout');
+        }, 3000);
+        console.log('Final función')
+    });
+    console.log(await miPromesa);
+}
+
+//Llamamos a la función
+funcionConPromesaYAwaitTimeout();
