a5c61b1231c89bfdd0fe0265d2bad3987f086970
Nacho0150 <ignacioibanez01@gmail.com>
2025-08-31 18:04:34 -0300

    Clase 03 Java

---
+++ Cuarto Semestre/Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
@@ -0,0 +1,28 @@
+package utn.estudiantes.servicio;
+
+import utn.estudiantes.modelo.Estudiante;
+
+import java.util.List;
+
+public class EstudianteServicio implements IEstudianteServicio {
+
+    @Override
+    public List<Estudiante> listarEstudiantes() {
+        return List.of();
+    }
+
+    @Override
+    public Estudiante buscarEstudiantePorId(Integer idEstudiante) {
+        return null;
+    }
+
+    @Override
+    public void guardarEstudiante(Estudiante estudiante) {
+
+    }
+
+    @Override
+    public void eliminarEstudiante(Estudiante estudiante) {
+
+    }
+}
