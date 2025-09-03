16a2710bed6f5e4f7edebf5b9f2ba522d2956a07
Nacho0150 <ignacioibanez01@gmail.com>
2025-09-03 20:31:35 -0300

    Clase 04 Java

--- Cuarto Semestre/Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
+++ Cuarto Semestre/Java/Leccion01/estudiantes/src/main/java/utn/estudiantes/servicio/EstudianteServicio.java
@@ -1,28 +1,36 @@
 package utn.estudiantes.servicio;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import utn.estudiantes.modelo.Estudiante;
+import utn.estudiantes.repositorio.EstudianteRepositorio;
 
 import java.util.List;
 
+@Service
 public class EstudianteServicio implements IEstudianteServicio {
+    @Autowired
+    private EstudianteRepositorio estudianteRepositorio;
 
     @Override
     public List<Estudiante> listarEstudiantes() {
-        return List.of();
+        List<Estudiante> estudiantes = estudianteRepositorio.findAll();
+        return estudiantes;
     }
 
     @Override
     public Estudiante buscarEstudiantePorId(Integer idEstudiante) {
-        return null;
+        Estudiante estudiante = estudianteRepositorio.findById(idEstudiante).orElse(null);
+        return estudiante;
     }
 
     @Override
     public void guardarEstudiante(Estudiante estudiante) {
-
+        estudianteRepositorio.save(estudiante);
     }
 
     @Override
     public void eliminarEstudiante(Estudiante estudiante) {
-
+        estudianteRepositorio.delete(estudiante);
     }
 }
